76151244c2304ea4425e33c18d0a72509ecb8016
Guilherme Paschoal <gabpaschoal@outlook.com>
2021-11-25 20:39:11 -0300

    cleaning code

--- FileUpload/Controllers/UploadController.cs
+++ FileUpload/Controllers/UploadController.cs
@@ -1,42 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Net.Http.Headers;
+namespace FileUpload.Controllers;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
-namespace FileUpload.Controllers
+[Route("api/[controller]")]
+[ApiController]
+public class UploadController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UploadController : ControllerBase
+    [HttpPost]
+    public async Task<IActionResult> Index(
+        [FromForm] IList<IFormFile> files,
+        [FromQuery] string param1,
+        [FromQuery] string param2)
     {
-        [HttpPost]
-        public async Task<IActionResult> Index(
-            [FromForm] IList<IFormFile> files,
-            [FromQuery] string param1,
-            [FromQuery] string param2)
+        var dirUploads = Path.Combine(Directory.GetCurrentDirectory(), "Uploads");
+        Directory.CreateDirectory(dirUploads);
+
+        foreach (IFormFile file in files)
         {
-            var dirUploads = Path.Combine(Directory.GetCurrentDirectory(), "Uploads");
-            Directory.CreateDirectory(dirUploads);
+            if (file.Length <= 0)
+                continue;
 
-            foreach (IFormFile file in files)
+            try
             {
-                if (file.Length <= 0)
-                    continue;
-
-                try
-                {
-                    using FileStream filestream = System.IO.File.Create(Path.Combine(dirUploads, file.FileName));
-                    await file.CopyToAsync(filestream);
-                    filestream.Flush();
-                }
-                catch (Exception ex)
-                {
-                    return BadRequest(ex.Message);
-                }
-
+                using FileStream filestream = System.IO.File.Create(Path.Combine(dirUploads, file.FileName));
+                await file.CopyToAsync(filestream);
+                filestream.Flush();
             }
-
-            return Ok();
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
+
+        return Ok();
     }
 }
